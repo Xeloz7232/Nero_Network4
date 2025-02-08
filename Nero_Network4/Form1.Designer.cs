e509e8ce8edaa9afe7fb0c07169139f1e070e432
Xeloz7232 <110711170+Xeloz7232@users.noreply.github.com>
2025-02-08 17:42:53 +0700

    Initial commit

---
+++ Nero_Network4/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Nero_Network4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bGive = new System.Windows.Forms.Button();
+            this.bCorrupt = new System.Windows.Forms.Button();
+            this.bUntrain = new System.Windows.Forms.Button();
+            this.pbSample = new System.Windows.Forms.PictureBox();
+            this.bPic1 = new System.Windows.Forms.Button();
+            this.bPic2 = new System.Windows.Forms.Button();
+            this.bPic3 = new System.Windows.Forms.Button();
+            this.bPic4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.bAuto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSample)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bGive
+            // 
+            this.bGive.Location = new System.Drawing.Point(10, 9);
+            this.bGive.Margin = new System.Windows.Forms.Padding(2);
+            this.bGive.Name = "bGive";
+            this.bGive.Size = new System.Drawing.Size(80, 23);
+            this.bGive.TabIndex = 0;
+            this.bGive.Text = "Дать";
+            this.bGive.UseVisualStyleBackColor = true;
+            this.bGive.Click += new System.EventHandler(this.bGive_Click);
+            // 
+            // bCorrupt
+            // 
+            this.bCorrupt.Location = new System.Drawing.Point(10, 38);
+            this.bCorrupt.Margin = new System.Windows.Forms.Padding(2);
+            this.bCorrupt.Name = "bCorrupt";
+            this.bCorrupt.Size = new System.Drawing.Size(80, 23);
+            this.bCorrupt.TabIndex = 0;
+            this.bCorrupt.Text = "Испортить";
+            this.bCorrupt.UseVisualStyleBackColor = true;
+            this.bCorrupt.Click += new System.EventHandler(this.bCorrupt_Click);
+            // 
+            // bUntrain
+            // 
+            this.bUntrain.Location = new System.Drawing.Point(10, 67);
+            this.bUntrain.Margin = new System.Windows.Forms.Padding(2);
+            this.bUntrain.Name = "bUntrain";
+            this.bUntrain.Size = new System.Drawing.Size(80, 23);
+            this.bUntrain.TabIndex = 0;
+            this.bUntrain.Text = "Разобучить";
+            this.bUntrain.UseVisualStyleBackColor = true;
+            this.bUntrain.Click += new System.EventHandler(this.bUntrain_Click);
+            // 
+            // pbSample
+            // 
+            this.pbSample.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbSample.Location = new System.Drawing.Point(101, 14);
+            this.pbSample.Margin = new System.Windows.Forms.Padding(2);
+            this.pbSample.Name = "pbSample";
+            this.pbSample.Size = new System.Drawing.Size(100, 100);
+            this.pbSample.TabIndex = 1;
+            this.pbSample.TabStop = false;
+            // 
+            // bPic1
+            // 
+            this.bPic1.Location = new System.Drawing.Point(210, 9);
+            this.bPic1.Name = "bPic1";
+            this.bPic1.Size = new System.Drawing.Size(75, 23);
+            this.bPic1.TabIndex = 2;
+            this.bPic1.Text = "Овен";
+            this.bPic1.UseVisualStyleBackColor = true;
+            this.bPic1.Click += new System.EventHandler(this.bPic1_Click);
+            // 
+            // bPic2
+            // 
+            this.bPic2.Location = new System.Drawing.Point(210, 38);
+            this.bPic2.Name = "bPic2";
+            this.bPic2.Size = new System.Drawing.Size(75, 23);
+            this.bPic2.TabIndex = 2;
+            this.bPic2.Text = "Рак";
+            this.bPic2.UseVisualStyleBackColor = true;
+            this.bPic2.Click += new System.EventHandler(this.bPic2_Click);
+            // 
+            // bPic3
+            // 
+            this.bPic3.Location = new System.Drawing.Point(210, 67);
+            this.bPic3.Name = "bPic3";
+            this.bPic3.Size = new System.Drawing.Size(75, 23);
+            this.bPic3.TabIndex = 2;
+            this.bPic3.Text = "Стрелец";
+            this.bPic3.UseVisualStyleBackColor = true;
+            this.bPic3.Click += new System.EventHandler(this.bPic3_Click);
+            // 
+            // bPic4
+            // 
+            this.bPic4.Location = new System.Drawing.Point(210, 96);
+            this.bPic4.Name = "bPic4";
+            this.bPic4.Size = new System.Drawing.Size(75, 23);
+            this.bPic4.TabIndex = 2;
+            this.bPic4.Text = "Весы";
+            this.bPic4.UseVisualStyleBackColor = true;
+            this.bPic4.Click += new System.EventHandler(this.bPic4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label.Location = new System.Drawing.Point(120, 123);
+            this.label.MaximumSize = new System.Drawing.Size(166, 13);
+            this.label.MinimumSize = new System.Drawing.Size(166, 13);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(166, 13);
+            this.label.TabIndex = 4;
+            this.label.Text = "Функционирование завершено";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // bAuto
+            // 
+            this.bAuto.Location = new System.Drawing.Point(10, 94);
+            this.bAuto.Margin = new System.Windows.Forms.Padding(2);
+            this.bAuto.Name = "bAuto";
+            this.bAuto.Size = new System.Drawing.Size(80, 23);
+            this.bAuto.TabIndex = 0;
+            this.bAuto.Text = "Авто-работа";
+            this.bAuto.UseVisualStyleBackColor = true;
+            this.bAuto.Click += new System.EventHandler(this.bAuto_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 145);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bPic4);
+            this.Controls.Add(this.bPic3);
+            this.Controls.Add(this.bPic2);
+            this.Controls.Add(this.bPic1);
+            this.Controls.Add(this.pbSample);
+            this.Controls.Add(this.bAuto);
+            this.Controls.Add(this.bUntrain);
+            this.Controls.Add(this.bCorrupt);
+            this.Controls.Add(this.bGive);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSample)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bGive;
+        private System.Windows.Forms.Button bCorrupt;
+        private System.Windows.Forms.Button bUntrain;
+        private System.Windows.Forms.PictureBox pbSample;
+        private System.Windows.Forms.Button bPic1;
+        private System.Windows.Forms.Button bPic2;
+        private System.Windows.Forms.Button bPic3;
+        private System.Windows.Forms.Button bPic4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button bAuto;
+    }
+}
+
---
+++ Nero_Network4/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nero_Network4
+{
+    public partial class Form1 : Form
+    {
+        static int lastSymbol;
+        const int _width = 100;
+        const int _height = 100;
+        static int N = _width * _height;
+        const int picNum = 4;
+        int[][] sample = new int[picNum][];
+        float[,] weights = new float[N, N];
+        int untrainings = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            label.Text = "";
+            label1.Text = $"Разобучений = {untrainings}";
+            for (int i = 0; i < picNum; i++)
+            {
+                Bitmap bm = (Bitmap)Image.FromFile($"{i}.png");
+                sample[i] = new int[N];
+                for (int j = 0; j < N; j++)
+                {
+                    sample[i][j] = bm.GetPixel(j % _width, j / _width) == Color.FromArgb(255, 0, 0, 0) ? 1 : -1;
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i; j < N; j++)
+                {
+                    if (i == j) weights[i, j] = 0;
+                    else
+                    {
+                        weights[i, j] = 0;
+                        for (int k = 0;  k < picNum;  k++)
+                        {
+                            weights[i, j] += sample[k][i] * sample[k][j];
+                        }
+                        weights[j, i] = weights[i, j];
+                    }
+                }
+            }
+        }
+        private void bPic1_Click(object sender, EventArgs e)
+        {
+            lastSymbol = 1;
+            pbSample.Image = Image.FromFile("0.png");
+        }
+        private void bPic2_Click(object sender, EventArgs e)
+        {
+            lastSymbol = 2;
+            pbSample.Image = Image.FromFile("1.png");
+        }
+        private void bPic3_Click(object sender, EventArgs e)
+        {
+            lastSymbol = 3;
+            pbSample.Image = Image.FromFile("2.png");
+        }
+        private void bPic4_Click(object sender, EventArgs e)
+        {
+            lastSymbol = 4;
+            pbSample.Image = Image.FromFile("3.png");
+        }
+
+        private void bGive_Click(object sender, EventArgs e)
+        {
+            label.Text = "";
+            int[] oldPicture = new int[N];
+            int[] newPicture = new int[N];
+            Bitmap bm = (Bitmap)pbSample.Image;
+            bool end;
+
+            for (int i = 0; i < N; i++)
+            {
+                oldPicture[i] = bm.GetPixel(i % _width, i / _width) == Color.FromArgb(255, 0, 0, 0) ? 1 : -1;
+            }
+            do
+            {
+                end = true;
+                for (int i = 0; i < N; i++)
+                {
+                    float sum = 0;
+                    for (int j = 0; j < N; j++)
+                    {
+                        sum += weights[i, j] * oldPicture[j];
+                    }
+                    if (sum < 0) newPicture[i] = -1;
+                    else if (sum > 0) newPicture[i] = 1;
+                    else newPicture[i] = oldPicture[i];
+                    if (newPicture[i] != oldPicture[i]) end = false;
+                }
+                for (int i = 0; i < N; i++)
+                {
+                    oldPicture[i] = newPicture[i];
+                }
+            } while (!end);
+            for (int i = 0; i < N; i++)
+            {
+                bm.SetPixel(i % _width, i / _width, newPicture[i] == 1 ? Color.FromArgb(255, 0, 0, 0) : Color.FromArgb(255, 255, 255, 255));
+            }
+            pbSample.Image = bm;
+            label.Text = "Функционирование завершено";
+        }
+
+        private void bCorrupt_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = (Bitmap)pbSample.Image;
+            Random r = new Random();
+            Point p;
+            for (int i = 0; i < N * 0.5f; i++)
+            {
+                p = new Point(r.Next(0, _width), r.Next(0, _height));
+                bm.SetPixel(p.X, p.Y, bm.GetPixel(p.X, p.Y) == Color.FromArgb(255, 255, 255, 255) ? Color.FromArgb(255, 0, 0, 0) : Color.FromArgb(255, 255, 255, 255));
+            }
+            pbSample.Image = bm;
+        }
+
+        private void bUntrain_Click(object sender, EventArgs e)
+        {
+            label.Text = "";
+            float epsilon = 0.1f;
+            Bitmap bm = (Bitmap)pbSample.Image;
+            int[] picture = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                picture[i] = bm.GetPixel(i % _width, i / _width) == Color.FromArgb(255, 0, 0, 0) ? 1 : -1;
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i; j < N; j++)
+                {
+                    if (i != j)
+                    {
+                        weights[i, j] -= epsilon * picture[i] * picture[j];
+                        weights[j, i] = weights[i, j];
+                    }
+                }
+            }
+            if (lastSymbol == 1) bPic1_Click(sender, e);
+            else if (lastSymbol == 2) bPic2_Click(sender, e);
+            else if (lastSymbol == 3) bPic3_Click(sender, e);
+            else bPic4_Click(sender, e);
+            untrainings++;
+            label.Text = "Разобучение завершено";
+            label1.Text = $"Разобучений = {untrainings}";
+        }
+
+        private void bAuto_Click(object sender, EventArgs e)
+        {
+            bool retest;
+            do
+            {
+                retest = false;
+                bPic1_Click(sender, e);
+                for (int k = 0; k < picNum; k++)
+                {
+                    bool end;
+                    do
+                    {
+                        end = true;
+
+                        int[] oldPicture = new int[N];
+                        Bitmap bm = (Bitmap)pbSample.Image;
+                        for (int i = 0; i < N; i++)
+                        {
+                            oldPicture[i] = bm.GetPixel(i % _width, i / _width) == Color.FromArgb(255, 0, 0, 0) ? 1 : -1;
+                        }
+
+                        for (int i = 0; i < 3; i++) bCorrupt_Click(sender, e);
+                        bGive_Click(sender, e);
+
+                        int[] newPicture = new int[N];
+                        bm = (Bitmap)pbSample.Image;
+                        for (int i = 0; i < N; i++)
+                        {
+                            newPicture[i] = bm.GetPixel(i % _width, i / _width) == Color.FromArgb(255, 0, 0, 0) ? 1 : -1;
+                        }
+
+                        for (int i = 0; i < N; i++)
+                        {
+                            if (newPicture[i] != oldPicture[i]) end = false;
+                        }
+                        if (!end)
+                        {
+                            bUntrain_Click(sender, e);
+                            if (lastSymbol != 1)
+                            {
+                                retest = true;
+                            }
+                        }
+                    } while (!end);
+                    if (lastSymbol != 4)
+                    {
+                        if (lastSymbol == 1) bPic2_Click(sender, e);
+                        else if (lastSymbol == 2) bPic3_Click(sender, e);
+                        else if (lastSymbol == 3) bPic4_Click(sender, e);
+                    }
+                }
+            } while (retest);
+        }
+    }
+}
